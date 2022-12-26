af1355274934a0c1cc33f68d5ab0b996901c5196
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-12-26 18:04:03 -0300

    praticando com operadores

--- src/main/java/com/javabasic/ualace/variaveis/Variaveis.java
+++ src/main/java/com/javabasic/ualace/variaveis/Variaveis.java
@@ -1,4 +1,11 @@
 package com.javabasic.ualace.variaveis;
 
 public class Variaveis {
+
+    public static void main(String[] args) {
+           final float VALOR_PI = 3.14f;
+
+        System.out.println(VALOR_PI);
+    }
+
 }
